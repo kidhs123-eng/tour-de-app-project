d2937512dc26e4fe6bae5fc6089d5bb2739875a1
kidhs123-eng <kidhs123@gmail.com>
2026-09-18 19:56:21 +0200

    html-fix

--- services/api/app/main.py
+++ services/api/app/main.py
@@ -8,12 +8,12 @@ app = FastAPI(title="Academy API")
 BASE_DIR = Path(__file__).resolve().parent
 
 
-HTML_PATH = BASE_DIR.parent / "frontend" / "index.html"
+INDEX_PATH = BASE_DIR / "frontend" / "templates" / "index.html"
 
 
 @app.get("/")
 def read_root():
-    return FileResponse(HTML_PATH)
+    return FileResponse(INDEX_PATH)
 
 
 @app.get("/api/health")
