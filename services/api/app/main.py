01debc41119e0994657aefe479dbec6c15ef420f
kidhs123-eng <kidhs123@gmail.com>
2026-09-18 19:47:37 +0200

    html-fix

--- services/api/app/main.py
+++ services/api/app/main.py
@@ -4,15 +4,11 @@ from fastapi.responses import FileResponse
 
 app = FastAPI(title="Academy API")
 
-# Автоматический поиск файла index.html
-BASE_DIR = Path(__file__).resolve().parent  # Папка app/
 
-# 1. Путь для Docker (когда папка frontend скопирована в /app/frontend)
+BASE_DIR = Path(__file__).resolve().parent
+
+
 HTML_PATH = BASE_DIR.parent / "frontend" / "index.html"
-
-# 2. Резервный путь для локального запуска из PyCharm
-if not HTML_PATH.exists():
-    HTML_PATH = BASE_DIR.parent.parent.parent / "frontend" / "index.html"
 
 
 @app.get("/")
